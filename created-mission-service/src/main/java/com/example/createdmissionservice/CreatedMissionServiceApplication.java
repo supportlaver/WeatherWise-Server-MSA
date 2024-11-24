730626573f483e87e0aee0d79fb0ebbccefa7045
supportlaver <ak4686@naver.com>
2024-11-24 20:03:32 +0900

    feat: 멀티 모듈로 분리

---
+++ created-mission-service/src/main/java/com/example/createdmissionservice/CreatedMissionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.createdmissionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreatedMissionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreatedMissionServiceApplication.class, args);
+    }
+
+}
