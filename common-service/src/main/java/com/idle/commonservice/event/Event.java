1150722db8ba6b6dfc6affb0ad78c25f8eec3b61
supportlaver <ak4686@naver.com>
2024-12-05 21:30:28 +0900

    feat: common-service 에 Event 관련 코드 추가

---
+++ common-service/src/main/java/com/idle/commonservice/event/Event.java
@@ -0,0 +1,14 @@
+package com.idle.commonservice.event;
+
+public abstract class Event {
+    private long timestamp;
+
+    public Event() {
+        this.timestamp = System.currentTimeMillis();
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+}
